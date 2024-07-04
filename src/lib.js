e48c0b0ddd49045af817f7e389db1cc5fb5fff58
Jacob Taylor <jtlikesps4@icloud.com>
2024-07-03 21:42:34 -0500

    Wrote helper functions in lib.js

--- src/lib.js
+++ src/lib.js
@@ -1,16 +1,58 @@
 import fs from 'fs'
 
-export const chooseRandom = () => {
-  // TODO implement chooseRandom
+export const chooseRandom = (array = [], numItems) => {
+  if (array.length <= 1) return array;
+  numItems = numItems >= 1 && numItems <= array.length ? numItems : Math.floor(Math.random() * array.length) + 1;
+  return Array.from({ length: numItems }, () => array[Math.floor(Math.random() * array.length)]);
 }
 
-export const createPrompt = () => {
-  // TODO implement createPrompt
-}
+export const createPrompt = ({ numQuestions = 1, numChoices = 2 } = {}) => {
+  const prompts = [];
 
-export const createQuestions = () => {
-  // TODO implement createQuestions
-}
+  for (let i = 1; i <= numQuestions; i++) {
+    prompts.push({
+      type: 'input',
+      name: `question-${i}`,
+      message: `Enter question ${i}`
+    });
+    for (let j = 1; j <= numChoices; j++) {
+      prompts.push({
+        type: 'input',
+        name: `question-${i}-choice-${j}`,
+        message: `Enter answer choice ${j} for question ${i}`
+      });
+    }
+  }
+
+  return prompts;
+};
+
+export const createQuestions = (questions = {}) => {
+  const questionObjects = [];
+  let questionNumber = 1;
+
+  while (`question-${questionNumber}` in questions) {
+    const question = questions[`question-${questionNumber}`];
+    const choices = [];
+    let choiceNumber = 1;
+
+    while (`question-${questionNumber}-choice-${choiceNumber}` in questions) {
+      choices.push(questions[`question-${questionNumber}-choice-${choiceNumber}`]);
+      choiceNumber++;
+    }
+
+    questionObjects.push({
+      type: 'list',
+      name: `question-${questionNumber}`,
+      message: question,
+      choices
+    });
+
+    questionNumber++;
+  }
+
+  return questionObjects;
+};
 
 export const readFile = path =>
   new Promise((resolve, reject) => {
